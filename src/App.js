7b8ca11f3f5715b13025fe83d59bcb254412b062
Fellipe Horta Parreiras <fhparreiras@gmail.com>
2022-03-23 09:09:08 -0300

    bg e titulo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import logo from './Star_Wars-Logo.svg';
+import FilterContext from './context/FilterContext';
 
 function App() {
+  const INITIAL_STATE = {
+    data: [],
+  };
+
   return (
-    <span>Hello, App!</span>
+    <FilterContext.Provider value={ INITIAL_STATE }>
+      <h1>
+        <img src={ logo } alt="logo-star-wars" />
+      </h1>
+    </FilterContext.Provider>
   );
 }
 
